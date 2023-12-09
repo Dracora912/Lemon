ec77fb33cedef67c56f1b8740a2377f83c5604f9
derfin22 <35113663+derfin22@users.noreply.github.com>
2023-12-09 18:16:39 -0500

    The final set of scripts

---
+++ Final Scripts/Observer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;
+    public GameObject parent;
+    public GameEnding gameEnding;
+    private PlayerMovement Shield;
+    bool m_IsPlayerInRange;
+    private void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        if (parent == null)
+        {
+            Debug.LogWarning("A gameObject does not have an attached parent, scripts may not work as intended.");
+        }
+        Shield = player.GetComponent<PlayerMovement>();
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.transform == player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+    void Update()
+    {
+        bool hasShield = Shield.shield;
+        Debug.Log(hasShield);
+        if(m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray (transform.position, direction);
+            RaycastHit raycastHit;
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    if (hasShield == false)
+                    {
+                        gameEnding.CaughtPlayer();
+                    }
+                    else
+                    {
+                        Shield.shield = false;
+                        Destroy(parent);
+                    }                      
+                }
+            }
+        }
+    }
+}
---
+++ Final Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float normalSpeed = 5f;
+    public float boostedSpeed = 10f;  // Speed when holding spacebar
+    public bool shield;
+    public GameObject Shield_Aura;
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+        shield = false;
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Shield_Aura.SetActive(shield);
+        
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        float currentSpeed = Input.GetKey(KeyCode.Space) ? boostedSpeed : normalSpeed;
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * currentSpeed * Time.fixedDeltaTime);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        switch (collision.name)
+        {
+            case "Shield":
+                //I picked a powerup!
+                Destroy(collision.gameObject);
+                if (shield == false)
+                {
+                    shield = true;
+                }
+                break;
+        }
+
+    }
+    public void Update()
+    {
+        
+    }
+}
